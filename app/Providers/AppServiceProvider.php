e6534bf60bd8087677ccbc67cf83ada53bd23fcf
joselitopapi14 <enriquejo2002@gmail.com>
2025-12-13 10:46:43 -0500

    Force HTTPS URLs in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS in production when behind a proxy (Dokploy/Traefik)
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
